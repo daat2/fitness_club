5295e7c7d0139daa56ce7f019282a18b30a66683
daat2 <ibi.olu@gmail.com>
2021-01-30 01:16:01 +0000

    Added allauth templates and base template

--- fitness_club/settings.py
+++ fitness_club/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -101,7 +102,7 @@ WSGI_APPLICATION = 'fitness_club.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
