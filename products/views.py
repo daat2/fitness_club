287878a6d21eb7f20d1925a86aa3e021ec6200c5
daat2 <ibi.olu@gmail.com>
2021-02-23 15:28:54 +0000

    Add functional details to prodcut page

--- products/views.py
+++ products/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
-
 
 # Create your views here.
 
 def all_products(request):
-
-    """ A view showing all produts, search queries and sorting """
+    """ A view showing all products,search queries and sorting """
 
     products = Product.objects.all()
 
@@ -14,6 +12,16 @@ def all_products(request):
         'products': products,
     }
 
-
-
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view display individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
