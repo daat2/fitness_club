6ffbcad193ff100299504050e3e843b13644da27
daat2 <ibi.olu@gmail.com>
2021-03-01 19:03:03 +0000

    Add caching to settings

--- fitness_club/settings.py
+++ fitness_club/settings.py
@@ -182,6 +182,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    #Caching
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'daatfitness-club'
     AWS_S3_REGION_NAME = 'eu-west-2'
