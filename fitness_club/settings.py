c73a9a606af74beb0dd42dbd8bae5cc408e41104
daat2 <ibi.olu@gmail.com>
2021-03-01 13:21:54 +0000

    Add a Connection to heroku and disable collestatic

--- fitness_club/settings.py
+++ fitness_club/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'f7a)!+kjv7@ds6&(jpk6e@epq4i56g=bui*tv(ip589e@a#n$z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['daatfitness-club.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,13 +125,17 @@ WSGI_APPLICATION = 'fitness_club.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
